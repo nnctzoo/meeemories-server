033f5c562862ce030ccebe2873bdd03be2147faa
lamlam <lamlam4r@gmail.com>
2018-10-08 13:37:53 +0900

    Upload video

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -12,11 +12,27 @@ class ContentsController < ApplicationController
   def create
     file = params.require(:file)
 
-    case FileMagic.new(:mime_type).file(file.path)
+    mimetype = FileMagic.new(:mime_type).file(file.path)
+    case mimetype
     when /\Aimage\//
       # TODO
     when /\Avideo\//
-      # TODO
+      @video_transcoding = VideoTranscoding.new(:file => file)
+      if @video_transcoding.save
+        @media = Video.create
+        @source = @media.sources.create({
+          width: 0,
+          height: 0,
+          mime_type: mimetype
+        })
+
+        @content = Content.new
+        @content.media = @media
+        @content.save
+      else
+        # TODO: error handle
+        logger.error(@video_transcoding.errors)
+      end
     else
       head 400 and return
     end
