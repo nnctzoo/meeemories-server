932d3371231623709aa1185260f47ef264319be8
lamlam <lamlam4r@gmail.com>
2018-10-08 13:37:53 +0900

    Upload video
    Conflicts:
          app/controllers/contents_controller.rb

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -15,7 +15,22 @@ class ContentsController < ApplicationController
       picture = PictureCreator.new(file: file, mime_type: mime_type).run
       @detail = picture_path(picture)
     when /\Avideo\//
-      # TODO
+      @video_transcoding = VideoTranscoding.new(:file => file)
+      if @video_transcoding.save
+        @media = Video.create
+        @source = @media.sources.create({
+          width: 0,
+          height: 0,
+          mime_type: mime_type
+        })
+
+        @content = Content.new
+        @content.media = @media
+        @content.save
+      else
+        # TODO: error handle
+        logger.error(@video_transcoding.errors)
+      end
     else
       head 400 and return
     end
