dda9611f7611db34036bb067ef6134db0ffd316c
lamlam <lamlam4r@gmail.com>
2018-10-08 16:36:43 +0900

    Use x namespace
    Conflicts:
          app/views/contents/create.json.jbuilder
          config/environments/development.rb
          config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
   config.x.s3.bucket = 'meeemories-server-production'
 
   # AWS CONFIG
-  config.aws_region = 'ap-northeast-1'
-  config.aws_et_pipeline_id = '1537175214386-j9egwy'
-  config.aws_et_preset_id_system_web = '1351620000001-000020'
+  config.x.aws.region = 'ap-northeast-1'
+  config.x.aws.et_preset_id_system_web = '1351620000001-000020'
 end
