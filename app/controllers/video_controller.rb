d92ec98cd0813d3f3c736356bbd329e2225d1d6f
lamlam <lamlam4r@gmail.com>
2018-10-11 20:12:45 +0900

    Check SNS request header

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,28 +1,36 @@
 class VideoController < ApplicationController
   def create
-    @result = params.require(:outputs)[0].permit(:key, :thumbnailPattern, :width, :height)
+    if request.headers['x-amz-sns-message-type'] == 'SubscriptionConfirmation'
+      @subscribe_url = params.permit(:SubscribeURL)
+      logger.debug "Subscribe URL: #{@subscribe_url}"
+      render json: {}, status: 200
+    elsif request.headers['x-amz-sns-message-type'] == 'Notification'
+      @result = params.require(:outputs)[0].permit(:key, :thumbnailPattern, :width, :height)
 
-    @media = Video.create
-    # video source
-    @media.sources.create({
-      width: @result[:width],
-      height: @result[:height],
-      mime_type: 'video/mp4',
-      url: "#{Rails.configuration.x.aws.s3_url}/#{@result[:key]}"
-    })
-    # thumbnail source
-    thumb = @result[:thumbnailPattern].gsub(/\{count\}/,'00001')
-    @media.sources.create({
-      width: @result[:width],
-      height: @result[:height],
-      mime_type: 'image/png',
-      url: "#{Rails.configuration.x.aws.s3_url}/#{thumb}.png"
-    })
+      @media = Video.create
+      # video source
+      @media.sources.create({
+        width: @result[:width],
+        height: @result[:height],
+        mime_type: 'video/mp4',
+        url: "#{Rails.configuration.x.aws.s3_url}/#{@result[:key]}"
+      })
+      # thumbnail source
+      thumb = @result[:thumbnailPattern].gsub(/\{count\}/,'00001')
+      @media.sources.create({
+        width: @result[:width],
+        height: @result[:height],
+        mime_type: 'image/png',
+        url: "#{Rails.configuration.x.aws.s3_url}/#{thumb}.png"
+      })
 
-    @content = Content.new
-    @content.media = @media
-    @content.save
-    
-    render :create, formats: :json
+      @content = Content.new
+      @content.media = @media
+      @content.save
+      
+      render :create, formats: :json
+    else
+      render json: {}, status: 400
+    end
   end
 end
