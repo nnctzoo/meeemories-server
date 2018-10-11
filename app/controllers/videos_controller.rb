86dcc01a30607b44ab2afebb99ee8e589cb2aa85
lamlam <lamlam4r@gmail.com>
2018-10-12 01:58:53 +0900

    Verify SNS authentication

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,6 +5,13 @@ class VideosController < ApplicationController
   end
 
   def create
+    if Rails.env.production?
+      verifier = Aws::SNS::MessageVerifier.new
+      unless verifier.authentic?(request.body.string)
+        head 400 and return
+      end
+    end
+
     VideoCreator.new(params.require(:Message)).run
     head 200
   end
