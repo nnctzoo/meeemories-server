886f32498a93eb1bf02ce2cf54f8e7e8567b2ac8
kirikiriyamama <kirikiriyamama@gmail.com>
2018-10-12 14:48:46 +0900

    Fix message format

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -15,32 +15,37 @@ describe 'videos' do
       job = create(:video_transcoding_job)
 
       expect {
-        post '/videos', Message: JSON.generate({
-          state: 'COMPLETED',
-          version: '2012-09-25',
-          jobId: job.key,
-          pipelineId: 'xxxxx',
-          input: {
-            key: 'xxxxx',
-            frameRate: 'auto',
-            resolution: 'auto',
-            aspectRatio: 'auto',
-            interlaced: 'auto',
-            container: 'auto'
-          },
-          inputCount: 1,
-          outputs: [{
-            id: 1,
-            presetId: 'xxxxx',
-            key: 'xxxxx',
-            thumbnailPattern: 'xxxxx-{count}',
-            rotate: '0',
-            status: 'Complete',
-            duration: '10',
-            width: 640,
-            height: 480
-          }]
-        })
+        post '/videos', JSON.generate(
+          Message: JSON.generate(
+            state: 'COMPLETED',
+            version: '2012-09-25',
+            jobId: job.key,
+            pipelineId: 'xxxxx',
+            input: {
+              key: 'xxxxx',
+              frameRate: 'auto',
+              resolution: 'auto',
+              aspectRatio: 'auto',
+              interlaced: 'auto',
+              container: 'auto'
+            },
+            inputCount: 1,
+            outputs: [{
+              id: 1,
+              presetId: 'xxxxx',
+              key: 'xxxxx',
+              thumbnailPattern: 'xxxxx-{count}',
+              rotate: '0',
+              status: 'Complete',
+              duration: '10',
+              width: 640,
+              height: 480
+            }]
+          )
+        ),
+        {
+          'CONTENT_TYPE' => 'application/json'
+        }
       }.to  change(Content, :count).by(1)
        .and change(Video, :count).by(1)
        .and change(Source, :count).by(3) # Video, Thumbnail, Resized thumbnail
