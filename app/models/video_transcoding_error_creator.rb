4850298bd622aef11f7c4c613e6b4c4599630123
kirikiriyamama <kirikiriyamama@gmail.com>
2018-10-12 19:31:24 +0900

    Remove the meaningless variable

--- app/models/video_transcoding_error_creator.rb
+++ app/models/video_transcoding_error_creator.rb
@@ -1,14 +1,13 @@
 class VideoTranscodingErrorCreator
   def initialize(message)
     @message = JSON.parse(message)
-    @error_code = @message['errorCode']
   end
 
   def run
     verify_message!
 
     job = VideoTranscodingJob.find_by!(key: @message['jobId'])
-    VideoTranscodingError.create!(error_code: @error_code, video_transcoding_id: job.video_transcoding_id)
+    VideoTranscodingError.create!(error_code: @message['errorCode'], video_transcoding_id: job.video_transcoding_id)
   end
 
   private
